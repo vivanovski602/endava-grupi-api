50af0d6e7fbb8cfac447d9fc221bf81512da6576
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-05-28 22:35:03 +0200

    ControllerDivision2

--- endavaRestApi/Controllers/UserController.cs
+++ endavaRestApi/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using log4net.Config;
+﻿using endavaRestApi.Data;
+using endavaRestApi.Repositories;
+using log4net.Config;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,16 +10,16 @@ namespace endavaRestApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IShopRepository _shopRepository;
-        public ShopController(IShopRepository shopRepository)
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
         {
-            _shopRepository = shopRepository;
+            _userRepository = userRepository;
             XmlConfigurator.Configure(new FileInfo("log4net.config"));
         }
         [HttpPost("user/Add")]
         public async Task<ActionResult<User>> CreateUser(User user)
         {
-            var createdUser = await _shopRepository.AddUser(user);
+            var createdUser = await _userRepository.AddUser(user);
             return CreatedAtAction(nameof(GetUser), new { id = createdUser.Id }, createdUser);
 
 
@@ -27,20 +29,20 @@ namespace endavaRestApi.Controllers
         public async Task<ActionResult<User>> GetUser(int id)
         {
 
-            return await _shopRepository.Get(id);
+            return await _userRepository.Get(id);
         }
 
         [HttpPost("user/Reset-Password")]
         public async Task<ActionResult> ResetPassword(ResetPasswordRequest resetRequest)
         {
             //Check if username exists
-            var user = await _shopRepository.GetUserByName(resetRequest.Name);
+            var user = await _userRepository.GetUserByName(resetRequest.Name);
             if (user == null)
             {
                 return NotFound("Invalid username or e-mail!");
             }
             //Check if Email exists
-            user = await _shopRepository.GetUserByEmail(resetRequest.Email);
+            user = await _userRepository.GetUserByEmail(resetRequest.Email);
             if (user == null)
             {
                 return NotFound("Invalid username or e-mail!");
@@ -57,7 +59,7 @@ namespace endavaRestApi.Controllers
             }
             //Reset password
             user.Password = resetRequest.NewPassword;
-            await _shopRepository.UpdateUser(user);
+            await _userRepository.UpdateUser(user);
             return Ok("Password reset successfully!");
         }
     }
